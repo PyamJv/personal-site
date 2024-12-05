19ce581d97860c5c09538a05ad5478bb0f10b8e1
fardin2000 <safikhanifardin@gmail.com>
2024-12-04 23:31:17 -0500

    Initial Commit to get CSS working

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,9 +12,9 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
+        <h2>Payaam Javid</h2>
         <p>
-          <a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a>
+          <a href="mailto:jpayaam@gmail.com">jpayaam@gmail.com</a>
         </p>
       </header>
     </section>
